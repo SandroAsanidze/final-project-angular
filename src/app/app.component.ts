107a39407e7440ce14d1f89b38234aaffd208f86
Sandro <wrek.yamom@gmail.com>
2023-10-17 14:37:24 +0400

    add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy } from '@angular/core';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './features/header/header.component';
+import { FooterComponent } from './features/footer/footer.component';
 
 @Component({
   selector: 'app-root',
+  standalone:true,
+  imports:[CommonModule,RouterModule,HeaderComponent,FooterComponent],
+  changeDetection:ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
