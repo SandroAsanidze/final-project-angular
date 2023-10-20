05f92d0a674374a1d0f688c1461fdbbef966afbc
Sandro <wrek.yamom@gmail.com>
2023-10-20 21:07:13 +0400

    add interface in weather service

--- src/app/features/weather/service/weather.service.ts
+++ src/app/features/weather/service/weather.service.ts
@@ -2,6 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+export interface Weather {
+  main: {
+    humidity:number;
+    temp:number;
+  };
+  name:string;
+  weather:[
+    {
+      description:string;
+      main:string;
+    }
+  ];
+  wind: {
+    speed:number;
+  };
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +27,7 @@ export class WeatherService {
   constructor(private http:HttpClient) { }
   APIkey = 'aba09526722cb0d5c860492573a98768'
 
-  getWeatherData(city:string):Observable<any> {
-    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.APIkey}`);
+  getWeatherData(city:string):Observable<Weather> {
+    return this.http.get<Weather>(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.APIkey}`);
   }
 }
